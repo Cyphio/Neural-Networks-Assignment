05a5a001dfdf9b76dfa617a0e68b7c0686504814
Cyphio <Harry@mbr-online.co.uk>
2021-05-20 14:34:07 +0100

    Final commit

--- Classifier.py
+++ Classifier.py
@@ -9,6 +9,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torchvision.utils import make_grid
 from torch.utils.data import SubsetRandomSampler
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
@@ -18,10 +19,15 @@ import random
 import functools
 import operator
 
+# The ten classes of the CIFAR10 dataset
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
+"""
+The Classifier class provides methods for generating training, validation and testing data, calculating Cross-Entropy
+accuracy, as well as training, testing and loading models.
+"""
 class Classifier:
+    # Constructor for Classifier class
     def __init__(self, data, TRAIN_VAL_SPLIT, EPOCHS, BATCH_SIZE, LEARNING_RATE, loss_func, optimizer):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         print(f"RUNNING ON: {self.device}")
@@ -44,8 +50,7 @@ class Classifier:
         self.transform_data_augmentation = transforms.Compose([transforms.RandomCrop(32, padding=4),
                                                                transforms.RandomHorizontalFlip(),
                                                                transforms.ToTensor(),
-                                                               transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                                                                    (0.2023, 0.1994, 0.2010)), ])
+                                                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
         self.transform_normal = transforms.Compose([transforms.ToTensor(),
                                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -56,12 +61,14 @@ class Classifier:
         self.valloader = dataloaders['VAL']
         self.testloader = dataloaders['TEST']
 
+    # imshow is used to show images. Is used in report to depict training images with/without data augmentation
     def imshow(self, img):
-        img = img / 2 + 0.5  # unnormalize
+        img = img / 2 + 0.5
         npimg = img.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.show()
 
+    # get_dataloaders is used to generate and return training, testng and validation datasets
     def get_dataloaders(self):
         test_dataset = datasets.CIFAR10(root=f'./{self.data}', train=False, download=True,
                                         transform=self.transform_normal)
@@ -86,11 +93,13 @@ class Classifier:
                                   drop_last=True),
                 "TEST": DataLoader(test_dataset, batch_size=1)}
 
+    # multi_acc returns the accuracy of a distribution of predicted logits against the ground truth distribution
     def multi_acc(self, y_pred, y_test):
         _, y_pred_tags = torch.max(torch.log_softmax(y_pred, dim=1), dim=1)
         correct_pred = (y_pred_tags == y_test).float()
         return torch.round(correct_pred.sum() / len(correct_pred) * 100)
 
+    # train_model trains the model with set of defined training parameters
     def train_model(self, train_params, save_model=False, save_path="ANN_MODELS", save_name=None, epoch_per_save=10):
         model = CNN_Model(train_params)
         model.to(self.device)
@@ -167,13 +176,15 @@ class Classifier:
                 'loss': loss_stats['train'][-1], 'acc': accuracy_stats['train'][-1]},
                 f"{save_path}/{save_name}_epoch{self.EPOCHS}.pth")
 
-    def load_model(self, model_path):
+    # load_model loads a saved model and returns it
+    def load_model(self, model_path, params):
         checkpoint = torch.load(model_path)
-        model = self.model_class
+        model = CNN_Model(params)
         model.to(self.device)
         model.load_state_dict(checkpoint['model_state_dict'])
         return model
 
+    # test_model is used to test a model against the testing data, and prints a classification report
     def test_model(self, model):
         y_pred, y_ground_truth = [], []
         with torch.no_grad():
@@ -187,8 +198,12 @@ class Classifier:
                 y_ground_truth.append(y_test_batch.cpu().numpy())
         print(classification_report(y_ground_truth, y_pred, zero_division=0))
 
-
+"""
+The CNN_Model class implements the convolutional neural network model described in our report. The class also provides
+methods for forward-propagation.
+"""
 class CNN_Model(nn.Module):
+    # Constructor for CNN_Model class
     def __init__(self, params):
         nn.Module.__init__(self)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -211,6 +226,7 @@ class CNN_Model(nn.Module):
         # Need to hard code the num of in_channels for fully connected final layer
         self.fc_out = nn.Linear(8192, len(classes))
 
+    # forward is called with each network propagation to calculates the output Tensors from the input Tensors
     def forward(self, inputs):
         x = inputs
         for i in range(len(self.conv_layer)):
@@ -227,7 +243,7 @@ if __name__ == "__main__":
     train_val_split = 0.4
     epochs = 15
     epochs_per_save = 5
-    batch_size = 128
+    batch_size = 64
     loss_func = nn.CrossEntropyLoss()
     optimizer = optim.Adam
 
@@ -243,10 +259,17 @@ if __name__ == "__main__":
 
     ann = Classifier(data, train_val_split, epochs, batch_size, params.get("learning_rate"), loss_func, optimizer)
 
-    save_name = "batch-size-128"
+    save_name = "standard"
     ann.train_model(params, save_model=True, save_path=f"ANN_MODELS/{save_name}",
                     save_name=save_name, epoch_per_save=epochs_per_save)
 
-    # model_path = "ANN_MODELS/MLP/MLP_h-layer-width-64/MLP_h-layer-width-64_epoch15.pth"
-    # model = ann.load_model(model_path)
+    # Below loads a model and tests ir. Need to adjust above params dict to the architecture of model being used:
+    # model_path = "ANN_MODELS/standard/standard_epoch15.pth"
+    # model = ann.load_model(model_path, params)
     # ann.test_model(model)
+
+    # Below generates a grid of images in one batch & shows them along with their labels:
+    # dataiter = iter(ann.trainloader)
+    # images, labels = dataiter.next()
+    # ann.imshow(make_grid(images))
+    # print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
